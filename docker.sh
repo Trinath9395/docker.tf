1a00db7867573864741969d600a8563ff3012c2e
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-31 09:51:26 +0530

    terraform

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 
 # Expand disk
 growpart /dev/nvme0n1 4 || true
@@ -17,3 +19,13 @@ systemctl enable --now docker
 
 # Add ec2-user to docker group
 usermod -aG docker ec2-user
+
+#kubectl installation
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+#eksctl
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /usr/local/bin
